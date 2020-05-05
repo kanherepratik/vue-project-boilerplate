8c2a0f389525b2d0453788a13dca7f42eb1fcc6f
nikhil8_sharma <nikhilx58@gmail.com>
2020-05-05 13:13:19 +0530

    VUE: Radio Button

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="home">
         <todo-component />
+        <radio-button />
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import TodoComponent from '@/components/TodoComponent.vue';
+import RadioButton from '../../components/RadioButton.vue';
 
 export default {
     name: 'Home',
     components: {
-        'todo-component': TodoComponent
+        'todo-component': TodoComponent,
+        'radio-button': RadioButton
     }
 };
 </script>
