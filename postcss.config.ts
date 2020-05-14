2ea006a78322778d375d8db1c9cfa4b14d9d044c
Abhishek Aggarwal <abhishek2.aggarwal@paytmmall.com>
2020-05-14 15:55:34 +0530

    Linting issues fixed in mater

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,34 +1,28 @@
-const path = require('path');
+import path = require('path');
 
 module.exports = {
   plugins: {
     'postcss-import': {
-      resolve(id, basedir) {
+      resolve(id, basedir): string {
         // resolve alias @css, @import '@css/style.css'
         // because @css/ has 5 chars
-        if (/^@css/.test(id)) {
+        if (id.startsWith('@css')) {
           return path.resolve('./src/assets/styles/css', id.slice(5));
         }
         // resolve alias @pcss, @import '@pcss/style.css'
         // because @pcss/ has 6 chars
-        if (/^@pcss/.test(id)) {
-          return path.resolve(
-            './src/assets/styles/pcss',
-            id.slice(6),
-          );
+        if (id.startsWith('@pcss')) {
+          return path.resolve('./src/assets/styles/pcss', id.slice(6));
         }
         // resolve alias @pcss, @import '@common/style.pcss'
         // because @common/ has 8 chars
-        if (/^@common/.test(id)) {
-          return path.resolve(
-            './src/common/assets/styles/pcss',
-            id.slice(8),
-          );
+        if (id.startsWith('@common')) {
+          return path.resolve('./src/common/assets/styles/pcss', id.slice(8));
         }
 
         // resolve node_modules, @import '~normalize.css/normalize.css'
         // similar to how css-loader's handling of node_modules
-        if (/^~/.test(id)) {
+        if (id.startsWith('~')) {
           return path.resolve('./node_modules', id);
         }
 
@@ -45,26 +39,22 @@ module.exports = {
     'postcss-discard-empty': {},
     'postcss-discard-unused': {},
     'postcss-url': {
-      url(assets, dir) {
+      url(assets, dir): string {
         // get index of src folder from the absolute path of file folder
-        var srcIndex = dir.file.lastIndexOf('/ulip-frontend');
+        const srcIndex = dir.file.lastIndexOf('/ulip-frontend');
         // var srcIndex = dir.file.indexOf('/src');
         // resolve alias @images, url('@images/checkmark.svg')
-        if (/^@images/.test(assets.url) && srcIndex >= 0) {
+        if (assets.url.startsWith('@images') && srcIndex >= 0) {
           // base path in src folder
-          var basePath = 'public/images/';
+          const basePath = 'public/images/';
           // var basePath = 'assets/images/';
           // get number of folders a path needs to go up to the src folder
           // path is the file folder path from where the alias is called
-          var upstreamDirCount = (
-            dir.file.slice(srcIndex).match(/\//g) || []
-          ).length;
+          const upstreamDirCount = (dir.file.slice(srcIndex).match(/\//g) || []).length;
           // upstream path
-          var finalUpstreamPath = './';
+          let finalUpstreamPath = './';
           if (upstreamDirCount > 1) {
-            var finalUpstreamPath = Array(upstreamDirCount).join(
-              '../',
-            );
+            finalUpstreamPath = Array(upstreamDirCount).join('../');
           }
           // return new path with upstream path and the base path
           // 8 because @images/ has 8 characters
