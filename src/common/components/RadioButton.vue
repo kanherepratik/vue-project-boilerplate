29e2bb0502e663bd4733b00e6a00ec6358d6a022
Nickx58 <nikhilx58@gmail.com>
2020-05-20 10:34:54 +0530

    Vue: Radio Button Moved to common Components

---
+++ src/common/components/RadioButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="radioButton">
+    <div class="radioButton__box" @click="onRadioClicked"></div>
+    <div v-if="radioActive === value" class="radioButton--tick">{{ '&#x2714;' }}</div>
+    <div v-if="label" class="radioButton__label" @change="onRadioClicked">{{ label }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'RadioButton',
+  model: {
+    prop: 'radioActive',
+    event: 'onRadioUpdate',
+  },
+  props: {
+    value: String,
+    label: String,
+    disabled: Boolean,
+    radioActive: String,
+  },
+  methods: {
+    onRadioClicked(): void {
+      if (this.disabled) {
+        return;
+      }
+      this.$emit('onRadioUpdate', this.value);
+    },
+  },
+});
+</script>
+
+<style scoped>
+.radioButton {
+  display: flex;
+  flex: 1;
+  margin: 10px;
+}
+.radioButton__box {
+  min-width: 20px;
+  min-height: 20px;
+  max-height: 20px;
+  max-width: 20px;
+  border: 2px solid black;
+}
+.radioButton__label {
+  margin-left: 10px;
+  text-align: left;
+}
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="home">
     <todo-component />
-    <radio-component
-      v-for="(item, index) in items"
-      v-model="radioActive"
-      :key="index"
-      :value="item.value"
-      :label="item.label"
-    />
-    <p>Active Value: {{ radioActive }}</p>
   </div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
 import TodoComponent from '@/components/TodoComponent.vue';
-import RadioComponent from '@/components/RadioButton.vue';
 import Vue from 'vue';
 
 interface IData {} // local interface for data properties
@@ -24,25 +15,7 @@ export default Vue.extend({
   name: 'Home',
   components: {
     'todo-component': TodoComponent,
-    'radio-component': RadioComponent,
   },
-  data: () => ({
-    radioActive: '',
-    items: [
-      {
-        value: '1',
-        label: 'one',
-      },
-      {
-        value: '2',
-        label: 'Two',
-      },
-      {
-        value: '3',
-        label: 'Three',
-      },
-    ],
-  }),
   computed: {},
   methods: {},
 });
