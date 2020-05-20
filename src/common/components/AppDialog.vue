f474eb619dd4bf879cfc3ee09c5988cfa2c14498
Abhishek Aggarwal <abhishek2.aggarwal@paytmmall.com>
2020-05-20 13:30:34 +0530

    Added example for dialog using portal-vue

---
+++ src/common/components/AppDialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <portal-target name="destination">
+    <!--
+    This component can be located anwhere in your App.
+    The slot content of the above portal component will be rendered here.
+    -->
+  </portal-target>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { PortalTarget } from 'portal-vue';
+
+// local interface for data properties
+interface IAppDialogData {}
+
+export default Vue.extend({
+  name: 'AppDialog',
+  components: {
+    'portal-target': PortalTarget,
+  },
+  data: (): IAppDialogData => ({}),
+  computed: {},
+  methods: {},
+});
+</script>
--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -7,6 +7,23 @@
       <div v-for="(todo, index) in todoList" :key="index">
         {{ todo }}
       </div>
+      <portal v-if="showModal" to="destination">
+        <div class="overlay">
+          <div class="modal">
+            <h1>
+              Are you sure you want to remove this phone from your list?
+            </h1>
+            <div>
+              <button type="button" @click="closeModal">
+                Cancel
+              </button>
+              <button type="button">
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+      </portal>
     </div>
   </div>
 </template>
@@ -14,16 +31,22 @@
 <script lang="ts">
 import mixins from '@/mixins/mixinHelper';
 import todoMixin from '@/mixins/todoMixin';
+import { Portal } from 'portal-vue';
 
 // local interface for data object
 interface IData {
   task: string;
+  showModal: boolean,
 }
 
 export default mixins(todoMixin).extend({
   name: 'TodoComponent',
+  components: {
+    'portal': Portal,
+  },
   data: (): IData => ({
     task: '',
+    showModal: false,
   }),
   computed: {
     todoList: {
@@ -33,10 +56,36 @@ export default mixins(todoMixin).extend({
     },
   },
   methods: {
+    toggleModal(): void {
+      this.showModal = !this.showModal;
+    },
+    closeModal(): void {
+      this.toggleModal();
+    },
     onAddTodo(): void {
+      this.toggleModal();
       this.mixinOutput(this.task);
       this.$store.actionsHelper.addTodo(this.task);
     },
   },
 });
 </script>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  background: rgba(0, 0, 0, 0.75);
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  background: #fff;
+  width: 400px;
+  height: 200px;
+}
+</style>
