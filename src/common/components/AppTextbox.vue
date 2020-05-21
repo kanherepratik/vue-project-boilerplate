988bb5029177fab38b4bb8155f3c12ee1b561eaf
Abhishek Aggarwal <abhishek2.aggarwal@paytmmall.com>
2020-05-21 13:23:21 +0530

    Textbox component added

---
+++ src/common/components/AppTextbox.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="appTextbox">
+    <div class="textbxoContainer">
+      <div v-if="label" class="textbxoContainer__label">{{ label }}</div>
+      <!--
+        triggered on any checkbox click
+        @event onChange
+      -->
+      <input
+        class="textbxoContainer__input"
+        v-model="value"
+        :type="inputType"
+        :autofocus="autoFocus"
+        :placeholder="placeholder"
+        :maxlength="maxlength"
+        :disabled="disabled"
+        @input="onChange"
+        @focus="onFocus"
+        @blur="onBlur"
+      />
+      <div v-if="clearable" class="textbxoContainer__clearIcon">{{ clearIcon }}</div>
+    </div>
+    <div v-if="!validation.isValid" class="textboxErrorMsg">{{ errorMessage || validation.message }}</div>
+    <div v-else-if="message" class="textboxMsg">{{ message }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { IValidationRule, IValidation } from '../shared/interfaces';
+import { validationHandler } from '../shared/validations';
+
+// local interface for data properties
+interface IAppTextboxData {
+  value: string;
+  validation: IValidation; // To store the validation object
+}
+
+/**
+ * Props -
+ * inputType - d
+ */
+export default Vue.extend({
+  name: 'AppTextbox',
+  /**
+   * Model of the component
+   * prop contains the state of the component
+   */
+  model: {
+    prop: 'value',
+  },
+  props: {
+    /**
+     * Type of input
+     * @values String
+     * @default text
+     */
+    inputType: {
+      type: String,
+      default: 'text',
+    },
+    /**
+     * Focus state of input
+     * @values Boolean
+     * @default false
+     */
+    autoFocus: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Default value of the input
+     * @values String
+     */
+    defaultValue: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Max length of the input
+     * @values Number
+     */
+    maxlength: Number,
+    /**
+     * Flag to show the clear input symbol
+     * @values Boolean
+     * @default false
+     */
+    clearable: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Clear icon to be shown if the input can be clear using the icon
+     * @values String
+     * @default x
+     */
+    clearIcon: {
+      type: String,
+      default: 'x',
+    },
+    /**
+     * Disabled state of input
+     * @values Boolean
+     * @default false
+     */
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Label to be shown above the input
+     * @values String
+     */
+    label: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Placeholder to be shown on the input
+     * @values String
+     */
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Message to be shown below the input
+     * @values String
+     */
+    message: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Error message to be shown below the input in case of invalid state of input
+     * @values String
+     */
+    errorMessage: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Validations array of objects of type IValidationRule to valdiate the input
+     * @values Array<IValidationRule>
+     */
+    validations: {
+      type: Array as () => Array<IValidationRule>,
+      default: (): Array<IValidationRule> => [] as Array<IValidationRule>,
+    },
+  },
+  data: (): IAppTextboxData => ({
+    value: '',
+    validation: { isValid: true } as IValidation,
+  }),
+  created(): void {
+    // Updating value with the default value of the input
+    this.value = this.defaultValue;
+  },
+  methods: {
+    /**
+     * Calls the validationHandler to check the validations, whether the state of input is valid or not
+     * @returns boolean whether current state of the input is valid or not
+     */
+    isValid(): boolean {
+      this.validation = validationHandler(this.value, this.validations);
+      return this.validation.isValid;
+    },
+    /**
+     * onChange to be called in case of value change of input, emits onChange event
+     * @public
+     */
+    onChange(): void {
+      // Event to be discarded if input is disabled
+      if (this.disabled) {
+        return;
+      }
+      this.$emit('onChange', this.value);
+    },
+    /**
+     * onFocus to be called in case of input gets focus, emits onFocus event
+     * @public
+     */
+    onFocus(): void {
+      // Event to be discarded if input is disabled
+      if (this.disabled) {
+        return;
+      }
+      this.$emit('onFocus', this.value);
+    },
+    /**
+     * onBlur to be called in case of input gets blur, emits onBlur event
+     * @public
+     */
+    onBlur(): void {
+      // Event to be discarded if input is disabled
+      if (this.disabled) {
+        return;
+      }
+      this.$emit('onBlur', this.value);
+    },
+  },
+});
+</script>
