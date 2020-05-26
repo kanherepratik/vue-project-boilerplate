e7abbc07f1d731cc2a58d7a4f453c9af871b8f22
Nickx58 <nikhilx58@gmail.com>
2020-05-26 12:44:40 +0530

    Vue: Mobile Input added Validations

--- src/common/components/MobileInput.vue
+++ src/common/components/MobileInput.vue
@@ -3,69 +3,121 @@
     <input
       ref="input"
       type="tel"
-      v-model="phone"
+      v-model="value"
+      :autofocus="autoFocus"
       :disabled="disabled"
-      :id="inputId"
-      :name="name"
       :placeholder="placeholder"
       :maxlength="maxLen"
-      :required="required"
-      @input="onInput"
-      @keyup.enter="onEnter"
+      @input="onChange"
+      @focus="onFocus"
+      @blur="onBlur"
     />
+    <div v-if="!validation.isValid">{{ validation.message }}</div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { IValidationRule, IValidation } from '../shared/interfaces';
+import { validationHandler } from '../shared/validations';
 
-/*
- * Use validator for number and replace pattern
- */
+// local interface for data properties
+interface IMobileInput {
+  value: string;
+  validation: IValidation; // To store the validation object
+}
 
 export default Vue.extend({
   name: 'MobileInput',
+  model: {
+    prop: 'value',
+  },
   props: {
-    value: {
-      type: String,
-      default: '',
+    /**
+     * Validations array of objects of type IValidationRule to valdiate the input
+     * @values Array<IValidationRule>
+     */
+    validations: {
+      type: Array as () => Array<IValidationRule>,
+      default: (): Array<IValidationRule> => [] as Array<IValidationRule>,
     },
+    /**
+     * Focus state of input
+     * @values Boolean
+     * @default false
+     */
+    autoFocus: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Disabled state of input
+     * @values Boolean
+     * @default false
+     */
     disabled: {
       type: Boolean,
       default: false,
     },
-    inputId: {
+    /**
+     * Placeholder to be shown on the input
+     * @values String
+     */
+    placeholder: {
       type: String,
       default: '',
     },
-    name: {
-      type: String,
-      default: 'telephone',
-    },
-    placeholder: {
-      type: String,
-      default: 'Enter Mobile Number',
-    },
-    required: {
-      type: Boolean,
-      default: false,
-    },
-    maxLen: {
-      type: Number,
-      default: 10,
-    },
+    /**
+     * Max length of the input
+     * @values Number
+     */
+    maxLen: Number,
   },
-  data() {
-    return {
-      phone: '',
-    };
-  },
+  data: (): IMobileInput => ({
+    value: '',
+    validation: { isValid: true } as IValidation,
+  }),
   methods: {
-    onInput(): void {
-      this.$emit('input', this.phone);
+    /**
+     * Calls the validationHandler to check the validations, whether the state of input is valid or not
+     * @returns boolean whether current state of the input is valid or not
+     */
+    isValid(): boolean {
+      this.validation = validationHandler(this.value, this.validations);
+      return this.validation.isValid;
     },
-    onEnter(): void {
-      this.$emit('enter');
+    /**
+     * onChange to be called in case of value change of input, emits onChange event
+     * @public
+     */
+    onChange(): void {
+      // Event to be discarded if input is disabled
+      if (this.disabled) {
+        return;
+      }
+      this.$emit('onSubmit', this.value);
+    },
+    /**
+     * onFocus to be called in case of input gets focus, emits onFocus event
+     * @public
+     */
+    onFocus(): void {
+      // Event to be discarded if input is disabled
+      if (this.disabled) {
+        return;
+      }
+      this.$emit('onFocus', this.value);
+    },
+    /**
+     * onBlur to be called in case of input gets blur, emits onBlur event
+     * @public
+     */
+    onBlur(): void {
+      // Event to be discarded if input is disabled
+      if (this.disabled) {
+        return;
+      }
+      this.$emit('onBlur', this.value);
     },
   },
 });
