8c12f987b50561448ea41d459eac94f3774e51b1
Nickx58 <nikhilx58@gmail.com>
2020-05-27 13:05:03 +0530

    Vue: Tooltip components

---
+++ src/common/components/ToolTip.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p class="popup-box" v-show="hover">{{ message }}</p>
+    <div @mouseover="mouseOver">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+/**
+ *
+ * List of Props
+ * 1. slot -> on which html element we want to show tooltip
+ * 2. Message -> Message we want to show on tooltip hover state
+ */
+
+export default Vue.extend({
+  name: 'ToolTip',
+  props: ['message'],
+  data: () => ({
+    hover: false,
+  }),
+  methods: {
+    /**
+     * To Hide tooltip on mover over
+     */
+    mouseOver(): void {
+      this.hover = !this.hover;
+    },
+  },
+});
+</script>
+<style scoped>
+.popup-box {
+  position: absolute;
+  left: 50%;
+  top: 50px;
+  transform: translateX(-50%);
+  background: #f0f8ff;
+  border-radius: 1px;
+  box-shadow: 1px 1px 15px 0 rgba(0, 0, 0, 0.2);
+  padding: 40px;
+  color: #555585;
+}
+</style>
