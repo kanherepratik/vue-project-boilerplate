c4e5e879fc76f77bd7ae80962aa36ffdb4e10c0c
Abhishek Aggarwal <abhishekdtumc@gmail.com>
2020-05-29 22:32:24 +0530

    Registration Input component added and textbox component updated

--- src/common/shared/constants.ts
+++ src/common/shared/constants.ts
@@ -6,3 +6,5 @@ export const SUFFIX_DIVISOR_MAP: ISuffixDivisorMap = {
   lakh: 100000,
   crore: 10000000,
 };
+
+export const MOTOR_REGISTRATION_REGEX = '^[A-Z]{2}[ -][0-9]{1,2}(?: [A-Z])?(?: [A-Z]*)? [0-9]{4}$';
